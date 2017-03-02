76d97e1324f5f3323b1cbfe758bd11bd273e34f4
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-03-02 16:31:43 +0100

    chore(package): Add version & pre-publish scripts

---
+++ versionist.conf.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var fs = require('fs');
+
+module.exports = {
+  subjectParser: 'angular',
+  editChangelog: true,
+  editVersion: false,
+  getGitReferenceFromVersion: 'v-prefix',
+  template: fs.readFileSync( __dirname + '/doc/CHANGELOG.hbs', 'utf8' ),
+  addEntryToChangelog: {
+    preset: 'prepend',
+    fromLine: 2
+  },
+  getIncrementLevelFromCommit: (commit) => {
+    if( commit.subject.type == 'feat' )
+      return 'minor';
+    return 'patch';
+  },
+  transformTemplateData: (data) => {
+
+    console.log( data )
+
+    data.features = data.commits.filter((commit) => {
+      return commit.subject.type === 'feat';
+    });
+
+    data.fixes = data.commits.filter((commit) => {
+      return commit.subject.type === 'fix';
+    });
+
+    data.misc = data.commits.filter((commit) => {
+      return ![ 'fix', 'feat' ].includes(commit.subject.type);
+    });
+
+    return data;
+
+  },
+}
