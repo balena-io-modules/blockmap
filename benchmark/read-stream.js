557e4913eb7528064c67584e6be882260e399a04
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-07-06 21:38:57 +0200

    chore(package): Add benchmarks
    This adds benchmarks for some BlockMap parts,
    to facilitate observing performance improvements.
    
    Change-Type: patch

---
+++ benchmark/read-stream.js
@@ -0,0 +1,43 @@
+const path = require( 'path' )
+const fs = require( 'fs' )
+const bench = require( 'nanobench' )
+const BlockMap = require( '..' )
+
+bench( 'fs.ReadStream', function( run ) {
+
+  const filename = path.join( __dirname, '..', 'test/data/bmap.img' )
+  const readStream = fs.createReadStream( filename )
+
+  run.start()
+
+  readStream.on( 'readable', function() {
+    var chunk = null
+    while( chunk = this.read() ) {
+      continue
+    }
+  })
+  .once( 'end', function() {
+    run.end()
+  })
+
+})
+
+bench( 'BlockMap.ReadStream', function( run ) {
+
+  const filename = path.join( __dirname, '..', 'test/data/bmap.img' )
+  const blockMap = BlockMap.create( require( '../test/data/version-2.0' ) )
+  const readStream = BlockMap.createReadStream( filename, blockMap )
+
+  run.start()
+
+  readStream.on( 'readable', function() {
+    var chunk = null
+    while( chunk = this.read() ) {
+      continue
+    }
+  })
+  .once( 'end', function() {
+    run.end()
+  })
+
+})
